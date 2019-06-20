e51555ec49c9309274c52176a750a2306468228a
Krisli Dimo <krislidimo@gmail.com>
2019-06-20 16:31:00 -0500

    initial setup

---
+++ school/src/main/java/com/lambdaschool/school/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.school.repository;
+
+import com.lambdaschool.school.model.Student;
+import org.h2.store.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long>
+{
+    List<Student> findByStudnameContainingIgnoreCase(String name, Pageable pageable);
+}
